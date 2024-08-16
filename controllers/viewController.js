5af5042a893dd2cf454aadfc4d998c012876fb07
KaiqueFj <kaiquelferraz@hotmail.com>
2024-08-15 23:34:12 -0300

    clean: cleaned the code on the viewController improving how fast the info is retrieved and used, reducing the number of variables

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -101,186 +101,86 @@ exports.getSpecificCoin = catchAsync(async (req, res, next) => {
       });
     }
 
-    const {
-      id,
-      symbol,
-      name,
-      description: { en: descriptionEn },
-      links: {
-        homepage,
-        whitepaper,
-        repos_url: { github },
+    const getTicker = await axios({
+      method: "GET",
+      url: `https://api.coingecko.com/api/v3/coins/${coin}/tickers?include_exchange_logo=true&depth=true`,
+      headers: {
+        accept: "application/json",
+        "x-cg-demo-api-key": process.env.API_KEY_Cry,
       },
-      image: { thumb, small, large },
-      market_data: {
-        current_price,
-        market_cap,
-        market_cap_rank,
-        total_volume,
-        high_24h,
-        low_24h,
-        price_change_24h,
-        price_change_percentage_24,
-        price_change_percentage_7d,
-        price_change_percentage_14d,
-        price_change_percentage_30d,
-        price_change_percentage_60d,
-        price_change_percentage_200d,
-        price_change_percentage_1y,
-        market_cap_change_24,
-        market_cap_change_percentage_24h,
-        price_change_24h_in_currency,
-        price_change_percentage_1h_in_currency,
-        price_change_percentage_24h_in_currency,
-        price_change_percentage_7d_in_currency,
-        price_change_percentage_14d_in_currency,
-        price_change_percentage_30d_in_currency,
-        price_change_percentage_60d_in_currency,
-        price_change_percentage_200d_in_currency,
-        price_change_percentage_1y_in_currency,
-        market_cap_change_percentage_24h_in_currency,
-        total_supply,
-        max_supply,
-        circulating_supply,
-        last_updated,
-        ath,
-        ath_date,
-        ath_change_percentage,
-        atl,
-        atl_date,
-        atl_change_percentage,
-      },
-      tickers,
-    } = coinData;
+    });
 
-    const formattedCurrentPrice = {
-      ...current_price,
-      brl_formatted: formatCurrency(current_price.brl),
-    };
-
-    const formattedMarketCap = {
-      ...market_cap,
-      marketCap_formatted: formatCurrency(market_cap.brl),
-    };
-
-    const formattedTotalVolume = {
-      ...total_volume,
-      marketVolume_formatted: formatCurrency(total_volume.brl),
-    };
-
-    const formattedTotalSupply = {
-      marketTotalSupply_formatted: formatCurrency(total_supply),
-    };
-
-    const formattedMaxSupply = {
-      marketMaxSupply_formatted: formatCurrency(max_supply),
-    };
-
-    const formattedCirculatingSupply = {
-      marketCirculatingSupply: formatCurrency(circulating_supply),
-    };
+    const coinTicker = getTicker.data.tickers;
 
     const totalPercentageOfCirculatingSupply =
-      (circulating_supply / max_supply) * 100;
+      (coinData.market_data.circulating_supply /
+        coinData.market_data.max_supply) *
+      100;
 
-    const total = formatCurrency(totalPercentageOfCirculatingSupply);
-
-    const lowPrice = {
-      ...low_24h,
-      formattedLowPrice: formatCurrency(low_24h.brl),
+    const coinDataInfo = {
+      id: coinData.id,
+      symbol: coinData.symbol,
+      name: coinData.name,
+      descriptionEn: formatDescription(coinData.description.en),
+      homepage: coinData.links.homepage,
+      whitepaper: coinData.links.whitepaper,
+      github: coinData.links.repos_url.github,
+      thumb: coinData.image.thumb,
+      small: coinData.image.small,
+      large: coinData.image.large,
+      current_price: formatCurrency(coinData.market_data.current_price.brl),
+      market_cap: formatCurrency(coinData.market_data.market_cap.brl),
+      market_cap_rank: coinData.market_data.market_cap_rank,
+      total_volume: formatCurrency(coinData.market_data.total_volume.brl),
+      high_24h: formatCurrency(coinData.market_data.high_24h.brl),
+      low_24h: formatCurrency(coinData.market_data.low_24h.brl),
+      price_change_24h: coinData.market_data.price_change_24h.brl,
+      price_change_percentage_24:
+        coinData.market_data.price_change_percentage_24h,
+      price_change_percentage_24h_in_currency:
+        coinData.market_data.price_change_percentage_24h_in_currency.brl,
+      market_cap_change_percentage_24h_in_currency:
+        coinData.market_data.market_cap_change_percentage_24h_in_currency.brl,
+      total_supply: formatCurrency(coinData.market_data.total_supply),
+      max_supply: formatCurrency(coinData.market_data.max_supply),
+      circulating_supply: formatCurrency(
+        coinData.market_data.circulating_supply
+      ),
+      ath: formatCurrency(coinData.market_data.ath.brl),
+      ath_date: formatDateWithRelativeTime(coinData.market_data.ath_date.brl),
+      ath_change_percentage: coinData.market_data.ath_change_percentage.brl,
+      atl: formatCurrency(coinData.market_data.atl.brl),
+      atl_date: formatDateWithRelativeTime(coinData.market_data.atl_date.brl),
+      atl_change_percentage: coinData.market_data.atl_change_percentage.brl,
+      total: formatCurrency(totalPercentageOfCirculatingSupply),
     };
 
-    const highPrice = {
-      ...high_24h,
-      formattedHighPrice: formatCurrency(high_24h.brl),
-    };
-
-    const allTimeHighPrice = {
-      ...ath,
-      formattedAllTimeHighPrice: formatCurrency(ath.brl),
-    };
-
-    const allTimeLowPrice = {
-      ...atl,
-      formattedAllTimeLowPrice: formatCurrency(atl.brl),
-    };
-
-    const maxDateFormatted = {
-      formattedAthDate: formatDateWithRelativeTime(ath_date.brl),
-    };
-    const lowDateFormatted = {
-      formattedAtlDate: formatDateWithRelativeTime(atl_date.brl),
-    };
-
-    const formattedTextDescription = formatDescription(descriptionEn);
-
-    const result = {
-      id,
-      symbol,
-      name,
-      descriptionEn: formattedTextDescription,
-      homepage,
-      whitepaper,
-      github,
-      thumb,
-      small,
-      large,
-      current_price: formattedCurrentPrice,
-      market_cap: formattedMarketCap,
-      market_cap_rank,
-      total_volume: formattedTotalVolume,
-      high_24h: highPrice,
-      low_24h: lowPrice,
-      price_change_24h,
-      price_change_percentage_24,
-      price_change_percentage_7d,
-      price_change_percentage_14d,
-      price_change_percentage_30d,
-      price_change_percentage_60d,
-      price_change_percentage_200d,
-      price_change_percentage_1y,
-      market_cap_change_24,
-      market_cap_change_percentage_24h,
-      price_change_24h_in_currency,
-      price_change_percentage_1h_in_currency,
-      price_change_percentage_24h_in_currency,
-      price_change_percentage_7d_in_currency,
-      price_change_percentage_14d_in_currency,
-      price_change_percentage_30d_in_currency,
-      price_change_percentage_60d_in_currency,
-      price_change_percentage_200d_in_currency,
-      price_change_percentage_1y_in_currency,
-      market_cap_change_percentage_24h_in_currency,
-      total_supply: formattedTotalSupply,
-      max_supply: formattedMaxSupply,
-      circulating_supply: formattedCirculatingSupply,
-      last_updated,
-      ath: allTimeHighPrice,
-      ath_date: maxDateFormatted,
-      ath_change_percentage,
-      atl: allTimeLowPrice,
-      atl_date: lowDateFormatted,
-      atl_change_percentage,
-      total,
-      tickers: tickers.map((ticker) => ({
-        base: ticker.base,
-        target: ticker.target,
-        marketName: ticker.market.name,
-        identifier: ticker.market.identifier,
-        has_trading_incentive: ticker.market.has_trading_incentive,
-        last: ticker.last,
-        volume: ticker.volume,
-        converted_last: ticker.converted_last,
-        converted_volume: ticker.converted_volume,
-        trust_score: ticker.trust_score,
-        trade_url: ticker.trade_url,
-      })),
-    };
+    // Process the tickers array
+    const formattedTickers = coinTicker.map((ticker) => ({
+      base: ticker.base,
+      target: ticker.target,
+      marketName: ticker.market.name,
+      marketIdentifier: ticker.market.identifier,
+      hasTradingIncentive: ticker.market.has_trading_incentive,
+      logo: ticker.market.logo,
+      lastPrice: formatCurrency(ticker.last),
+      volume: formatCurrency(ticker.volume),
+      trustScore: ticker.trust_score,
+      cost_to_move_up_usd: ticker.cost_to_move_up_usd,
+      cost_to_move_down_usd: ticker.cost_to_move_down_usd,
+      bidAskSpreadPercentage: ticker.bid_ask_spread_percentage,
+      tradeUrl: ticker.trade_url,
+      lastTradedAt: ticker.last_traded_at,
+      lastFetchAt: formatDateWithRelativeTime(ticker.last_fetch_at),
+      convertedLast: ticker.converted_last,
+      convertedVolume: ticker.converted_volume,
+    }));
 
     res.status(200).render("coin", {
-      title: `Overview of ${name}`,
-      coin: result,
-      coinName: result.id,
+      title: `Overview of ${coinData.name}`,
+      coin: coinDataInfo,
+      coinTickerData: formattedTickers,
+      coinName: coinDataInfo.id,
     });
   } catch (err) {
     console.error(err);
