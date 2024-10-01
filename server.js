0e7e082b12c55efa8342881df0f0221a9ad161f2
KaiqueFj <kaiquelferraz@hotmail.com>
2024-09-30 22:04:19 -0300

    feat: added the connection to the database

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
-const app = require("./app");
-const dotenv = require("dotenv");
+const app = require('./app');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 
-dotenv.config({ path: "./.env" });
+dotenv.config({ path: './.env' });
 
-process.on("uncaughtException", (err) => {
-  console.log("UncaughtException, shutting down...");
+process.on('uncaughtException', (err) => {
+  console.log('UncaughtException, shutting down...');
   console.log(err.name, err.message);
   process.exit(1);
 });
@@ -14,9 +15,18 @@ const server = app.listen(PORT, () => {
   console.log(`App running on port ${PORT}...`);
 });
 
-process.on("unhandledRejection", (err) => {
+const DB = process.env.DATABASE.replace(
+  '<db_password>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB)
+  .then(() => console.log(`DB connection successfully established`));
+
+process.on('unhandledRejection', (err) => {
   console.log(err.name, err.message);
-  console.log("Unhandled rejection, shutting down...");
+  console.log('Unhandled rejection, shutting down...');
   server.close(() => {
     process.exit(1);
   });
