bd88c00469a2c15eace7094447cdb80fecdbe93b
KaiqueFj <kaiquelferraz@hotmail.com>
2024-11-01 10:45:16 -0300

    style: added semantic names to the styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,17 +11,17 @@ module.exports = {
           'rgba(var(--primary-text-color)  /  var(--tw-divide-opacity))',
         'secondary-text-color': 'rgba(var(--secondary-text-color)) ',
         'third-text-color': 'rgba(var(--third-text-color))',
-        'hover-btn-color': 'rgba(var(--hover-btn-color))',
+        'hover-background-color': 'rgba(var(--hover-background-color))',
+        'hover-text-color': 'rgba(var(--hover-text-color))',
         'star-color': 'rgba(var(--star-color) /  var(--tw-divide-opacity))',
-        'main-blue-text-color': 'rgba(var(--main-blue-text-color))',
+        'fourth-text-color': 'rgba(var(--fourth-text-color))',
       },
       backgroundColor: {
         background: 'rgba(var(--background))',
-        'hover-soft-bg-text-color': 'rgba(var(--hover-soft-bg-text-color))',
+        'divider-color': 'rgba(var(--divider-color))',
       },
       divideColor: {
-        'third-text-color-gray':
-          'rgba(var(--third-text-color-gray) /  var(--tw-divide-opacity))', // Ensures opacity is applied
+        'divider-color': 'rgba(var(--divider-color))', // Ensures opacity is applied
       },
     },
   },
